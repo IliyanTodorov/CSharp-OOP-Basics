edf5b5b5b8e3abe3b70fa626a7c82a0e9915c42b
Iliyan Todorov <venomflow2018@gmail.com>
2018-10-16 16:54:21 +0300

    DEFINING CLASSES LAB

---
+++ 01. Defining Classes - Lab/BankAccount/StartUp.cs
@@ -0,0 +1,75 @@
+﻿namespace BankAccount
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var clients = new Dictionary<int, BankAccount>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var token = command.Split();
+
+                var currentCommand = token[0];
+                int id = int.Parse(token[1]);
+
+
+                if (currentCommand == "Create")
+                {
+
+                    CreateAccount(clients, id);
+                }
+                else if (currentCommand == "Deposit")
+                {
+                    if (AccountExist(clients, id))
+                    {
+                        clients[id].Deposit(decimal.Parse(token[2]));
+                    }
+                }
+                else if (currentCommand == "Withdraw")
+                {
+                    if (AccountExist(clients, id))
+                    {
+                        clients[id].Withdraw(decimal.Parse(token[2]));
+                    }
+                }
+                else
+                {
+                    if (AccountExist(clients, id))
+                    {
+                        Console.WriteLine($"Account ID{id}, balance {clients[id].Balance:f2}");
+                    }
+                }
+            }
+
+        }
+
+        private static void CreateAccount(Dictionary<int, BankAccount> clients, int id)
+        {
+            if (clients.ContainsKey(id))
+            {
+                Console.WriteLine("Account already exists");
+            }
+            else
+            {
+                var current = new BankAccount();
+                current.Id = id;
+                clients.Add(id, current);
+            }
+        }
+
+        static bool AccountExist(Dictionary<int, BankAccount> accounts, int id)
+        {
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ 01. Defining Classes - Lab/Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonClass
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            BankAccount obb = new BankAccount();
+            obb.Id = 1230001;
+            obb.Balance = 550.02m;
+            BankAccount ktb = new BankAccount();
+            ktb.Id = 40234134;
+            ktb.Balance = 3m;
+            List<BankAccount> accounts = new List<BankAccount> { obb, ktb };
+
+            Person stoyan = new Person("Stoyan", 23, accounts);
+
+            Console.WriteLine(stoyan.GetBalance());
+        }
+    }
+}
