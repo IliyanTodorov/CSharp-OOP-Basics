50578a1433327f1bc02e93cfa9f0c08776c0cfaa
Iliyan Todorov <venomflow2018@gmail.com>
2018-10-28 10:59:30 +0200

    ENCAPSULATION EXERCISE

---
+++ 06. Encapsulation - Exercise/01. ClassBox/Box.cs
@@ -0,0 +1,29 @@
+﻿public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double LateralSurfaceArea()
+    {
+        return 2 * length * height + 2 * width * height;
+    }
+
+    public double SurfaceArea()
+    {
+        return 2 * length * width + 2 * length * height + 2 * width * height;
+    }
+
+    public double Volume()
+    {
+        return length * width * height;
+    }
+}
+
---
+++ 06. Encapsulation - Exercise/01. ClassBox/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassBox
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
+        }
+    }
+}
