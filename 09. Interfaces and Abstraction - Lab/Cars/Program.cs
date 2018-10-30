409ab8a5c9411bf75c9a58e38bed164480c4bc21
Iliyan Todorov <venomflow2018@gmail.com>
2018-10-30 15:36:03 +0200

    INTERFACES AND ABSTRACTION LAB

---
+++ 09. Interfaces and Abstraction - Lab/Cars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main()
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model S", "White", 4);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+        }
+    }
+}
